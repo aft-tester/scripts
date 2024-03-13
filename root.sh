7641d204a6e8c94f9215a2e8675eb0d4b60bb4f8
aft-tester <159992523+aft-tester@users.noreply.github.com>
2024-03-13 09:40:33 +0300

    Create root.sh

---
+++ root.sh
@@ -0,0 +1,20 @@
+#! usr/bin/bash
+#
+apt update
+apt upgrade
+apt update
+apt install git
+source ~
+apt update
+apt install curl
+source ~
+apt update
+apt install snapd
+source ~
+apt update
+snap install code --classic
+source ~
+apt update
+snap install drawio
+source ~
+apt update
